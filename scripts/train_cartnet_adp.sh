bbe8012b8d5838b7845e12198a3401aeedabbb8e
Àlex Solé <jaume.alexandre.sole@upc.edu>
2024-10-23 10:06:41 +0200

    updated bash scripts

--- scripts/train_cartnet_adp.sh
+++ scripts/train_cartnet_adp.sh
@@ -1,5 +1,6 @@
 
-CUDA_VISIBLE_DEVICES=0 python ../main.py --seed 0 --name "CartNet" --model "CartNet" --dataset "ADP" --dataset_path "/scratch/g1alexs/ADP_DATASET"  \
+cd ..
+CUDA_VISIBLE_DEVICES=0 python main.py --seed 0 --name "CartNet" --model "CartNet" --dataset "ADP" --dataset_path "./dataset/ADP_DATASET"  \
                                     --wandb_project "CartNet Paper" --batch 4 --batch_accumulation 16 --lr 0.001 --epochs 50 \
                                     --augment
 
--- scripts/train_cartnet_jarvis.sh
+++ scripts/train_cartnet_jarvis.sh
@@ -1,6 +1,7 @@
 
 cd ..
 echo "--figshare_target can be any of this for Jarvis dataset: [optb88vdw_total_energy, optb88vdw_bandgap, mbj_bandgap, formation_energy_peratom, ehull]"
+
 CUDA_VISIBLE_DEVICES=0 python main.py --seed 0 --figshare_target "optb88vdw_bandgap" --name "jarvis_dft_3D_optb88vdw_bandgap_cartnet" --model "CartNet" --dataset "jarvis" --dataset_path "./dataset/jarvis/"  \
                                     --wandb_project "CartNet Paper Jarvis" --batch 64 --batch_accumulation 1 --lr 0.001 --epochs 500  
 
